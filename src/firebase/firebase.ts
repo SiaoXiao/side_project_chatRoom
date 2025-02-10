f07df8f756eff7cd258e71aa4b8e08db889c7f94
Keith <keith.lee@ehs.tw>
2025-02-10 21:44:07 +0800

    Remove .env from repository

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -5,7 +5,7 @@ import {getFirestore, addDoc, collection, doc, getDoc,setDoc, onSnapshot} from '
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBAES_API_KEY,
   authDomain: import.meta.env.VITE_FIREBAES_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBAES_PROJECJ_ID,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBAES_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBAES_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBAES_APP_ID
